73eaf4eb0675607540bea2fae30e61be2c0d5b8e
Ashish Kumar Guntipalli <as589490@dal.ca>
2024-06-20 17:35:57 -0300

    Made alignment changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,33 +16,37 @@ const Home = () => {
     >
       <Flex
         width={{ base: "100%", sm: "90%", md: "80%", lg: "70%" }}
-        flexDirection="column"
+        flexDirection={{ base: "column", lg: "row" }}
         alignItems="center"
         textAlign="center"
         backgroundColor="rgba(0, 0, 0, 0.4)"
         borderRadius="md"
         p={{ base: 4, sm: 6, md: 8 }}
+        justifyContent="space-around"
       >
-        <Text fontSize={{ base: "2xl", sm: "3xl", md: "4xl" }} mb={4}>
-          You are a <Text as="span" color="green.400">candidate</Text> looking for your dream job?
-        </Text>
-        <Text fontSize={{ base: "lg", sm: "xl", md: "2xl" }} mb={8}>
-          Start your job search now!
-        </Text>
-        
-        <Stack spacing={4} width="100%" maxWidth="600px" mb={8}>
-          <Input placeholder="Job title, Keywords" variant="filled" backgroundColor="white" color="white"/>
+        <Box flex="1" marginRight={{ lg: "4" }}>
+          <Text fontSize={{ base: "2xl", sm: "3xl", md: "4xl" }} mb={4}>
+            Are you <Text as="span" color="green.400">candidate</Text> looking for your dream job?
+          </Text>
+          <Text fontSize={{ base: "lg", sm: "xl", md: "2xl" }} mb={8}>
+            Start your job search now!
+          </Text>
+          
+          <Stack spacing={4} width="100%" maxWidth="600px" mb={8}>
+            <Input placeholder="Job title, Keywords" variant="filled" backgroundColor="white" color="black"/>
+            <Button colorScheme="green" size="lg">Search</Button>
+          </Stack>
+        </Box>
 
-          <Button colorScheme="green" size="lg">Search</Button>
-        </Stack>
-
-        <Text fontSize={{ base: "2xl", sm: "3xl", md: "4xl" }} mb={4}>
-          You are an <Text as="span" color="blue.400">employer</Text> looking for the perfect candidate?
-        </Text>
-        <Text fontSize={{ base: "lg", sm: "xl", md: "2xl" }} mb={8}>
-          Post your job now!
-        </Text>
-        <Button colorScheme="blue" size="lg">I want to recruit now!</Button>
+        <Box flex="1" marginLeft={{ lg: "4" }}>
+          <Text fontSize={{ base: "2xl", sm: "3xl", md: "4xl" }} mb={4}>
+            Are you an <Text as="span" color="orange.400">employer</Text> looking for the perfect candidate?
+          </Text>
+          <Text fontSize={{ base: "lg", sm: "xl", md: "2xl" }} mb={8}>
+            Post your job now!
+          </Text>
+          <Button colorScheme="orange" size="lg">I want to recruit now!</Button>
+        </Box>
       </Flex>
     </Box>
   );
