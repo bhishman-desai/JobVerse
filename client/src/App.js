39bb9f6af00cfd4a9964a3c617196240c3b40c4d
Jayrajsinh Mahavirsinh Jadeja <jy688645@dal.ca>
2024-06-24 16:22:57 -0300

    added contact us page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import { lazy } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Box } from '@chakra-ui/react';
-import withLayout from './components/layout/withLayout';
-import './App.css';
+import { lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
+import withLayout from "./components/layout/withLayout";
+import "./App.css";
 
-const Home = lazy(() => import('./pages/Home'));
-const AboutPage = lazy(() => import('./pages/About'));
-const FaqPage = lazy(() => import('./pages/Faq'));
+const Home = lazy(() => import("./pages/Home"));
+const AboutPage = lazy(() => import("./pages/About"));
+const FaqPage = lazy(() => import("./pages/Faq"));
+const ContactPage = lazy(() => import("./pages/Contact"));
 
 /* Auth Pages */
-const UserNamePage = lazy(() => import('./pages/auth/Username'));
-const PasswordPage = lazy(() => import('./pages/auth/Password'));
-const RegisterPage = lazy(() => import('./pages/auth/Register'));
-const ProfilePage = lazy(() => import('./pages/auth/Profile'));
-const RecoveryPage = lazy(() => import('./pages/auth/Recovery'));
-const ResetPage = lazy(() => import('./pages/auth/Reset'));
+const UserNamePage = lazy(() => import("./pages/auth/Username"));
+const PasswordPage = lazy(() => import("./pages/auth/Password"));
+const RegisterPage = lazy(() => import("./pages/auth/Register"));
+const ProfilePage = lazy(() => import("./pages/auth/Profile"));
+const RecoveryPage = lazy(() => import("./pages/auth/Recovery"));
+const ResetPage = lazy(() => import("./pages/auth/Reset"));
 
 /* Page Not Found */
-const PageNotFoundPage = lazy(() => import('./components/pageNotFound'));
+const PageNotFoundPage = lazy(() => import("./components/pageNotFound"));
 
 function App() {
   return (
@@ -27,8 +28,9 @@ function App() {
           <Route element={withLayout(Home)()} path="/" />
           <Route element={withLayout(AboutPage)()} path="/about-us" />
           <Route element={withLayout(FaqPage)()} path="/faq" />
+          <Route element={withLayout(ContactPage)()} path="/contact-us" />
 
-          { /*Auth Routes*/ }
+          {/*Auth Routes*/}
           <Route element={withLayout(UserNamePage)()} path="/login" />
           <Route element={withLayout(PasswordPage)()} path="/password" />
           <Route element={withLayout(RegisterPage)()} path="/register" />
@@ -36,9 +38,8 @@ function App() {
           <Route element={withLayout(RecoveryPage)()} path="/recovery" />
           <Route element={withLayout(ResetPage)()} path="/reset" />
 
-          { /*404 Page Not Found*/ }
+          {/*404 Page Not Found*/}
           <Route element={withLayout(PageNotFoundPage)()} path="*" />
-
         </Routes>
       </Router>
     </Box>
---
+++ client/src/pages/Contact.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+import {
+  Container,
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Button,
+  VStack,
+  Wrap,
+  WrapItem,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Textarea,
+} from "@chakra-ui/react";
+import { MdPhone, MdEmail, MdLocationOn, MdOutlineEmail } from "react-icons/md";
+import { BsPerson } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+const Contact = () => {
+  return (
+    <Container maxW="full" mt={0} centerContent overflow="hidden">
+      <Flex>
+        <Box
+          background={"#f4f6f7"}
+          color="black"
+          borderRadius="lg"
+          m={{ sm: 2, md: 16, lg: 10 }}
+          p={{ sm: 2, md: 4, lg: 6 }}
+        >
+          <Box p={4}>
+            <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
+              <WrapItem>
+                <Box>
+                  <Heading>Contact Us!</Heading>
+                  <Text mt={{ sm: 3, md: 3, lg: 5 }} color="black">
+                    Fill up the form below to contact
+                  </Text>
+                  <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
+                    <VStack pl={0} spacing={3} alignItems="flex-start">
+                      <Button
+                        size="md"
+                        height="48px"
+                        width="200px"
+                        variant="ghost"
+                        _hover={{ border: "2px solid #1C6FEB" }}
+                        leftIcon={<MdPhone color="#1970F1" size="20px" />}
+                      >
+                        +1-9999999999
+                      </Button>
+                      <Button
+                        size="md"
+                        height="48px"
+                        width="200px"
+                        variant="ghost"
+                        _hover={{ border: "2px solid #1C6FEB" }}
+                        leftIcon={<MdEmail color="#1970F1" size="20px" />}
+                      >
+                        hello@abc.com
+                      </Button>
+                      <Button
+                        size="md"
+                        height="48px"
+                        width="200px"
+                        variant="ghost"
+                        _hover={{ border: "2px solid #1C6FEB" }}
+                        leftIcon={<MdLocationOn color="#1970F1" size="20px" />}
+                      >
+                        Halifax, Canada
+                      </Button>
+                    </VStack>
+                  </Box>
+                  <Link to={"/"}>
+                    <Button> Go back!</Button>
+                  </Link>
+                </Box>
+              </WrapItem>
+              <WrapItem>
+                <Box bg="white" borderRadius="lg">
+                  <Box m={8} color="#0B0E3F">
+                    <VStack spacing={5}>
+                      <FormControl id="name">
+                        <FormLabel>Your Name</FormLabel>
+                        <InputGroup borderColor="#E0E1E7">
+                          <InputLeftElement pointerEvents="none">
+                            <BsPerson color="gray.800" />
+                          </InputLeftElement>
+                          <Input type="text" placeholder="Name" size="md" />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id="name">
+                        <FormLabel>Your Mail</FormLabel>
+                        <InputGroup borderColor="#E0E1E7">
+                          <InputLeftElement pointerEvents="none">
+                            <MdOutlineEmail color="gray.800" />
+                          </InputLeftElement>
+                          <Input type="text" placeholder="Email" size="md" />
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl id="name">
+                        <FormLabel>Message</FormLabel>
+                        <Textarea
+                          borderColor="gray.300"
+                          _hover={{
+                            borderRadius: "gray.300",
+                          }}
+                          placeholder="Enter your message here!"
+                        />
+                      </FormControl>
+                      <FormControl id="name" float="right">
+                        <Button
+                          variant="solid"
+                          bg="#0D74FF"
+                          color="white"
+                          _hover={{}}
+                        >
+                          Send Message
+                        </Button>
+                      </FormControl>
+                    </VStack>
+                  </Box>
+                </Box>
+              </WrapItem>
+            </Wrap>
+          </Box>
+        </Box>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Contact;
