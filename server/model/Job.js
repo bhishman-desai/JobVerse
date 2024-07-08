1de85c4d46de5693411c03df3993f55dd7900755
Ashish Kumar Guntipalli <as589490@dal.ca>
2024-07-08 09:18:24 -0300

    Add model for Job

---
+++ server/model/Job.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const jobSchema = new mongoose.Schema({
+    positionName: { type: String, required: true },
+    salary: { type: Number, required: true },
+    positionsAvailable: { type: Number, required: true },
+    jobDescription: { type: String, required: true },
+    resumeRequired: { type: Boolean, required: true },
+    coverLetterRequired: { type: Boolean, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+const Job = mongoose.model('Job', jobSchema);
+
+export default Job;
