41831901b41d8a652b6e425b6e66215ce9f080f9
SIVAPRAKASH CHITTU HARIHARAN <sv470985@dal.ca>
2024-07-29 21:30:44 -0300

    Edit apirouter

--- server/router/apiRoutes.js
+++ server/router/apiRoutes.js
@@ -4,7 +4,7 @@ import testRoute from "./api/testRoute.js";
 import RecjobMgmt from "./api/recruiter/jobMgmt.js";
 import jobSeekerRoutes from "./api/jobSeeker.js";
 import userRoutes from "./api/userRoute.js";
-
+import SearchFunc from "./api/search/searchFunc.js"
 const router = Router();
 
 router.use("/", authRoute);
